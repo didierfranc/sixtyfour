f33b895fff91e2b0a9405f2acd5c40483442ccb6
Didier Franc <mail@didierfranc.com>
2016-02-03 18:29:56 +0100

    Update sixtyfour.js

--- src/sixtyfour.js
+++ src/sixtyfour.js
@@ -1,1 +1,55 @@
+function prepareImage (id, q, w, h, callback) {
 
+var file = document.getElementById(id).files[0]
+
+  if ( file.type.match(/image.*/) ) {
+
+    var reader = new FileReader()
+    reader.readAsDataURL(file)
+
+    reader.onload = function() {
+
+      var img = new Image()
+      img.src = reader.result
+
+      img.onload = function(){
+
+        r = this.width / this.height
+
+        var canvas = document.createElement('canvas')
+        var ctx = canvas.getContext('2d')
+
+        if(h){
+          canvas.width = h*r
+          canvas.height = h
+          ctx.drawImage(img, 0, 0, h*r, h)
+        }
+
+        if(w){
+          canvas.width = w
+          canvas.height = w/r
+          ctx.drawImage(img, 0, 0, w, w/r)
+        }
+
+        var dataURL = canvas.toDataURL("image/jpeg", q/100)
+
+        callback(dataURL)
+
+      }
+    }
+  }
+}
+
+function displayImages ( array, id ) {
+
+  var parent = document.getElementById(id)
+  parent.innerHTML = ''
+
+  for (var i = 0; i < array.length; i++) {
+
+    var newImg = new Image()
+    newImg.src = array[i]
+    parent.appendChild(newImg)
+
+  }
+}
